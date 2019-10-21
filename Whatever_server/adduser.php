0ef97f0498858f8e19083ec0d156c8734f18b16e
kotkirobiamrr <56228551+kotkirobiamrr@users.noreply.github.com>
2019-10-21 19:28:33 +0200

    already working

--- Whatever_server/adduser.php
+++ Whatever_server/adduser.php
@@ -1,17 +1,19 @@
  <?php
+
 	$status = ['status' => 9];	//default status code: unidentified error
 
  	include ('includes/db_init.php');
- 	$post = json_decode(readfile('php://input'));
+ 	
+ 	$data = json_decode(file_get_contents('php://input'));
  	$stmt = $pdo -> prepare ('select name from users where name = ?');
- 	$stmt -> execute ([$post['name']]);
+ 	$stmt -> execute ([$data -> name]);
 
  	if ($stmt -> rowCount() == 0)	//username is available
  	{
 		//we're adding user to database
 		$stmt = $pdo -> prepare ('insert into users (name, password) values (:name, :pass)');
-		$post['password'] = password_hash ($post['password'], PASSWORD_DEFAULT);
-		$stmt -> execute ([':name' => $post['name'], ':pass' => $post['password']]);
+		$data -> password = password_hash ($data -> password, PASSWORD_DEFAULT);
+		$stmt -> execute ([':name' => $data -> name, ':pass' => $data -> password]);
 		if ($stmt -> rowCount() == 1)
 			$status = ['status' => 0];	//all right, only one row added
  	}
